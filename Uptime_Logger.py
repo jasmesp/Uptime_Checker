deee75271791939b27b96e4370a33ca4c1680dde
jasmesp <jasmes@gmail.com>
2022-03-05 15:29:20 -0500

    made it prettier.

--- Uptime_Logger.py
+++ Uptime_Logger.py
@@ -5,11 +5,12 @@ from time import sleep
 from pythonping import ping
 
 while True:
-    x = random.randint(1800, 3600)
-    print(x / 60)
-    y = ping('1.1.1.1', verbose=True)
+    sleep_duration = random.randint(1800, 3600)
+    sleep_duration_pretty = f'{round(sleep_duration / 60)} minutes {sleep_duration % 60} seconds'
+    print(sleep_duration_pretty)
+    check_ping = ping('1.1.1.1', verbose=True)
     uptime_log = open('uptime_check_log.txt', 'a')
     time_stamp = datetime.datetime.now()
     print(time_stamp)
-    uptime_log.writelines((str(y) + f'\nsleep time: {x} seconds.\n\n'))
-    sleep(x)
+    uptime_log.writelines((str(check_ping) + f'\nsleep time: {sleep_duration} seconds, or {sleep_duration_pretty}\n\n'))
+    sleep(sleep_duration)
